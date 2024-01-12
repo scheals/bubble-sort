bc309e680aed9892f4bdd9a0cc9c0593427bfd26
Scheals <95084627+scheals@users.noreply.github.com>
2024-01-12 22:23:08 +0100

    fix: improve project file structure

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -11,9 +11,3 @@ def bubble_sort(array)
   end
 array
 end
-
-p bubble_sort([4,3,78,2,0,2])
-# => [0,2,2,3,4,78]
-p bubble_sort([4,3,78,2,0,2].shuffle)
-p bubble_sort([9, 6, 5, 4, 3, 1])
-p bubble_sort([9, 6, 5, 4, 3, 1].shuffle)
